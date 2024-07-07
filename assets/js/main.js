c70ba5211914e8148c31e040c1a73de4c1027aea
AHSAN HABIB MUAZ <ahsanhabibmuaz@gmail.com>
2024-07-07 22:56:04 +0600

    supported for video tag

--- assets/js/main.js
+++ assets/js/main.js
@@ -116,3 +116,4 @@ function scrollUp() {
     if (this.scrollY >= 560) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
 }
 window.addEventListener('scroll', scrollUp)
+

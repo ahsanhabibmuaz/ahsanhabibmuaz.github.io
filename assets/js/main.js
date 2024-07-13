9cd0f6b06e79849a2590f29c501ac2f0356c5343
AHSAN HABIB MUAZ <ahsanhabibmuaz@gmail.com>
2024-07-13 22:21:45 +0600

    blocked inspect inc. cnt+sft+I

--- assets/js/main.js
+++ assets/js/main.js
@@ -117,3 +117,36 @@ function scrollUp() {
 }
 window.addEventListener('scroll', scrollUp)
 
+
+
+/* All inspect off */
+// disable right click
+document.oncontextmenu = function (e) {
+    e.preventDefault()
+    return false;
+}
+// disable somme additional key
+document.onkeydown = function (e) {
+    // disable f12
+    if (event.keyCode == 123) {
+        return false;
+    }
+    // disable ctrl+shift+I
+    if (e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) {
+        return false;
+    }
+    // disable ctrl+shift+C
+    if (e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0)) {
+        return false;
+    }
+    // disable ctrl+shift+j
+    if (e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) {
+        return false;
+    }
+    // disable ctrl+shift+U
+    if (e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) {
+        return false;
+    }
+}
+
+/* finished inspect  */
